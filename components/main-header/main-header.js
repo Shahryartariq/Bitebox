24e79c3754153961ea1a0118cffebaeda3388a20
Muhammad Shehryar <Shahryartariq57@gmail.com>
2026-01-05 02:37:21 +0500

    Added Community Page

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -5,6 +5,7 @@ import logoImg from '@/assets/logo.png';
 import classes from "@/components/main-header/main-header.module.css"
 import Image from 'next/image';
 import MainHeaderBackground from './main-header-background';
+import { NavLink } from './nav-link';
 
 const MainHeader = () => {
   return (
@@ -19,15 +20,11 @@ const MainHeader = () => {
         <nav className={classes.nav}>
         <ul>
           <li>
-            <Link href="/meals">
-                Browser Meals
-            </Link>
+            <NavLink href="/meals">Browse Meals</NavLink>
           </li>
 
           <li>
-            <Link href="/community">
-                Foodies Community
-            </Link>
+            <NavLink href="/community">BiteBox Community</NavLink>
           </li>
         </ul>
 
