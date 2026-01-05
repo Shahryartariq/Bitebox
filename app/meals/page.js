a1ceda662d9f219db7c2e0ee0eb3a03979b1941e
Muhammad Shehryar <Shahryartariq57@gmail.com>
2026-01-05 05:12:11 +0500

    Improve Loading using suspence

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,33 +1,35 @@
-import classes from '@/app/meals/page.module.css';
-import MealsGrid from '@/components/meals/meals-grid';
-import Link from 'next/link';
-import { getMeals } from '@/lib/meals';
+import classes from "@/app/meals/page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import Link from "next/link";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-const MealsPage = async () => {
+async function Meals() {
   const meals = await getMeals();
-  return (
-    <>
-    <header className={classes.header}>
-      <h1>
-        Delicious Meals, Created
-        <span className={classes.highlight}>by you</span> 
-      </h1>
-      <p>Choose your favorite recipe and cook it yourself. easy and fun!</p>
-      <p className={classes.cta}>
-        <Link href="/meals/share">
-          Share Your Favourite Recipe
-        </Link>
-      </p>
-    </header>
-
-    <main className={classes.main}>
-      <MealsGrid meals={meals} />
-    </main>
-    
-  
-
-    </>
-  )
+  return <MealsGrid meals={meals} />;
 }
 
-export default MealsPage
+const MealsPage = async () => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals, Created
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself. easy and fun!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favourite Recipe</Link>
+        </p>
+      </header>
+
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
+};
+
+export default MealsPage;
